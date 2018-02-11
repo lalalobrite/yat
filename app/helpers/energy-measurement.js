0b9472735af1f00fee74fc7ed67002e34e19fcf6
Lala Lobrite <lalalobrite@mail.com>
2018-02-11 10:14:25 -0500

    show negative units correctly

--- app/helpers/energy-measurement.js
+++ app/helpers/energy-measurement.js
@@ -1,15 +1,16 @@
 import { helper } from '@ember/component/helper';
 
 export function weightMeasurement(params/*, hash*/) {
-  if (params < 1000) {
+  const abs = Math.abs(params);
+  if (abs < 1000) {
     return `${(params / 1).toFixed(0)}ncal`;
-  } else if (params < 1000000){
+  } else if (abs < 1000000){
     return `${(params / 1000).toFixed(2)}μcal`;
-  } else if (params < 10000000){
+  } else if (abs < 10000000){
     return `${(params / 1000000).toFixed(2)}mcal`;
-  } else if (params < 100000000){
+  } else if (abs < 100000000){
     return `${(params / 10000000).toFixed(2)}ccal`;
-  } else if (params < 100000000000){
+  } else if (abs < 100000000000){
     return `${(params / 100000000).toFixed(2)}cal`;
   } else {
     return `${(params / 100000000000).toFixed(2)}kcal`;
--- app/helpers/weight-measurement.js
+++ app/helpers/weight-measurement.js
@@ -1,15 +1,16 @@
 import { helper } from '@ember/component/helper';
 
 export function weightMeasurement(params/*, hash*/) {
-  if (params < 1000) {
+  const abs = Math.abs(params);
+  if (abs < 1000) {
     return `${(params / 1).toFixed(0)}ng`;
-  } else if (params < 1000000){
+  } else if (abs < 1000000){
     return `${(params / 1000).toFixed(2)}μg`;
-  } else if (params < 10000000){
+  } else if (abs < 10000000){
     return `${(params / 1000000).toFixed(2)}mg`;
-  } else if (params < 100000000){
+  } else if (abs < 100000000){
     return `${(params / 10000000).toFixed(2)}cg`;
-  } else if (params < 100000000000){
+  } else if (abs < 100000000000){
     return `${(params / 100000000).toFixed(2)}g`;
   } else {
     return `${(params / 100000000000).toFixed(2)}kg`;
